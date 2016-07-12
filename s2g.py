ca3aab9872bf2cbd1e155a2aadeaf6ddd00b0738
Jordan Doorlag <jld73@students.calvin.edu>
2016-07-12 12:43:50 -0400

    Moved copy constructor generation
    The copy constructor is now generated in the genConstructorCode function.  It is generated there always, regardless of the presence of clone blocks. This could potentially be refined.

--- s2g.py
+++ s2g.py
@@ -475,16 +475,6 @@ def whenSpriteCloned(codeObj, tokens):
     cbStr += block(1, tokens) + "\n"  # add blank line after defn.
     codeObj.addToCbCode(cbStr)
 
-    # Generate a copy constructor too.
-    cbStr = "\n\n" + genIndent(1) + "// copy constructor, required for cloning\n"
-    cbStr += genIndent(1) + "public " + spriteName + "(" + \
-             spriteName + " other, int x, int y) {\n"
-    cbStr += genIndent(2) + "super(other, x, y);\n"
-    cbStr += genIndent(2) + "// add code here to copy any instance variables'\n"
-    cbStr += genIndent(2) + "// values from other to this.\n"
-    cbStr += genIndent(1) + "}\n\n"
-    codeObj.addToCbCode(cbStr)
-
 
 def whenKeyPressed(codeObj, key, tokens):
     """Generate code to handle the whenKeyPressed block.  key is
@@ -1205,7 +1195,7 @@ def genHeaderCode(outFile, spriteName):
 
 
 def genConstructorCode(outFile, spriteName, code):
-    """Generate code for the constructor.
+    """Generate code for the constructor and copy constructor.
     This code will include calls to initialize data, etc., followed by code
     to register callbacks for whenFlagClicked,
     whenKeyPressed, etc.
@@ -1215,7 +1205,14 @@ def genConstructorCode(outFile, spriteName, code):
     outFile.write(genIndent(1) + "{\n")
     # Write out the code that registers the callbacks.
     outFile.write(code)
-    outFile.write(genIndent(1) + "}\n");
+    outFile.write(genIndent(1) + "}\n\n");
+    # Add a copy constructor
+    outFile.write(genIndent(1) + "public " + spriteName + "(" + spriteName + " other, int x, int y)\n")
+    outFile.write(genIndent(1) + "{\n")
+    outFile.write(genIndent(2) + "super(other, x, y);\n")
+    outFile.write(genIndent(2) + "// add code here to copy any instance variables'\n")
+    outFile.write(genIndent(2) + "// values from other to this.\n")
+    outFile.write(genIndent(1) + "}\n")
 
 def genLoadCostumesCode(costumes, isBackdrop=False):
     """Generate code to load costumes from files for a sprite.
