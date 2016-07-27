e0e8badef382e8a7ed0b4d0953a792fdad27243c
Jordan Doorlag <jld73@students.calvin.edu>
2016-07-27 14:50:29 -0400

    Possible fix for #103

--- Scratch.java
+++ Scratch.java
@@ -628,6 +628,7 @@ public class Scratch extends Actor
         }
 
         public Integer get() { return (Integer) super.get(); }
+        public void set(Object newVal) { super.set((Integer) newVal); }
     }
     public class StringVar extends Variable {
 
@@ -636,6 +637,7 @@ public class Scratch extends Actor
         }
 
         public String get() { return (String) super.get(); }
+        public void set(Object newVal) { super.set((String) newVal); }
     }
     public class DoubleVar extends Variable {
 
@@ -644,6 +646,7 @@ public class Scratch extends Actor
         }
 
         public Double get() { return (Double) super.get(); }
+        public void set(Object newVal) { super.set((Double) newVal); }
     }
     public class BooleanVar extends Variable {
 
@@ -652,6 +655,7 @@ public class Scratch extends Actor
         }
 
         public Boolean get() { return (Boolean) super.get(); }
+        public void set(Object newVal) { super.set((Boolean) newVal); }
     }
 
     /**
